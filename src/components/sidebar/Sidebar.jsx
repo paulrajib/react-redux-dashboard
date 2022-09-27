293fc8053c329ffd3713a11d000d74aff430076a
Rajib_p <silicon.slice@gmail.com>
2022-09-27 20:00:51 +0600

    sidebar and top-navbar completed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -22,64 +22,77 @@ const Sidebar = () => {
 
             {/*<i className="fa fa-bars fa-2x toggle-btn" data-bs-toggle="collapse" data-bs-target="#menu-content"></i>*/}
           
-                <div className="menu-list">          
-                    <ul id="menu-content" className="menu-content collapse out">
-                        <li>
-                          <a href="#">
-                          <i className="fas fa-tachometer-alt fa-lg"></i> Dashboard
-                          </a>
-                        </li>
+            <div className="menu-list">          
+                <ul id="menu-content" className="menu-content collapse out">
+                    <li>
+                      <a href="/test">
+                      <i className="fas fa-tachometer-alt fa-lg"></i> Dashboard
+                      </a>
+                    </li>
 
-                        <li  data-bs-toggle="collapse" data-bs-target="#products" className="collapsed active">
-                          <a href="#"><i className="fab fa-studiovinari fa-lg"></i> UI Elements <span className="arrow"></span></a>
-                        </li>
-                        <ul className="sub-menu collapse" id="products">
-                            <li className="active"><a href="#">CSS3 Animation</a></li>
-                            <li><a href="#">General</a></li>
-                            <li><a href="#">Buttons</a></li>
-                            <li><a href="#">Tabs & Accordions</a></li>
-                            <li><a href="#">Typography</a></li>
-                            <li><a href="#">FontAwesome</a></li>
-                            <li><a href="#">Slider</a></li>
-                            <li><a href="#">Panels</a></li>
-                            <li><a href="#">Widgets</a></li>
-                            <li><a href="#">Bootstrap Model</a></li>
-                        </ul>
-
-
-                        <li data-bs-toggle="collapse" data-bs-target="#service" className="collapsed">
-                          <a href="#"><i className="fab fa-fort-awesome-alt fa-lg"></i> Services <span className="arrow"></span></a>
-                        </li>  
-                        <ul className="sub-menu collapse" id="service">
-                          <li>New Service 1</li>
-                          <li>New Service 2</li>
-                          <li>New Service 3</li>
-                        </ul>
-
-
-                        <li data-bs-toggle="collapse" data-bs-target="#new" className="collapsed">
-                          <a href="#"><i className="fab fa-pagelines fa-lg"></i> New <span className="arrow"></span></a>
-                        </li>
-                        <ul className="sub-menu collapse" id="new">
-                          <li>New New 1</li>
-                          <li>New New 2</li>
-                          <li>New New 3</li>
-                        </ul>
-
-
-                         <li>
-                          <a href="#">
-                          <i className="fas fa-user-tie fa-lg"></i> Profile
-                          </a>
-                          </li>
-
-                         <li>
-                          <a href="#">
-                          <i className="fa fa-users fa-lg"></i> Users
-                          </a>
-                        </li>
+                    <li  data-bs-toggle="collapse" data-bs-target="#products" className="collapsed active">
+                      <a href="/test"><i className="fab fa-studiovinari fa-lg"></i> UI Elements <span className="arrow"></span></a>
+                    </li>
+                    <ul className="sub-menu collapse" id="products">
+                        <li className="active"><a href="/test">CSS3 Animation</a></li>
+                        <li><a href="/test">General</a></li>
+                        <li><a href="/test">Buttons</a></li>
+                        <li><a href="/test">Tabs & Accordions</a></li>
+                        <li><a href="/test">Typography</a></li>
+                        <li><a href="/test">FontAwesome</a></li>
+                        <li><a href="/test">Slider</a></li>
+                        <li><a href="/test">Panels</a></li>
+                        <li><a href="/test">Widgets</a></li>
+                        <li><a href="/test">Bootstrap Model</a></li>
                     </ul>
-             </div>
+
+
+                    <li data-bs-toggle="collapse" data-bs-target="#service" className="collapsed">
+                      <a href="/test"><i className="fab fa-fort-awesome-alt fa-lg"></i> Services <span className="arrow"></span></a>
+                    </li>  
+                    <ul className="sub-menu collapse" id="service">
+                      <li>New Service 1</li>
+                      <li>New Service 2</li>
+                      <li>New Service 3</li>
+                    </ul>
+
+
+                    <li data-bs-toggle="collapse" data-bs-target="#new" className="collapsed">
+                      <a href="/test"><i className="fab fa-pagelines fa-lg"></i> New <span className="arrow"></span></a>
+                    </li>
+                    <ul className="sub-menu collapse" id="new">
+                      <li>New New 1</li>
+                      <li>New New 2</li>
+                      <li>New New 3</li>
+                    </ul>
+
+                    <li>
+                      <a href="/test">
+                        <i className="fa fa-users fa-lg"></i> Users
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="/test">
+                        <i className="fas fa-user-tie fa-lg"></i> Profile
+                      </a>
+                    </li>
+
+                    <li>
+                      <a href="/test">
+                        <i className="fas fa-sign-out-alt"></i> Logout
+                      </a>
+                    </li>
+
+
+                </ul>
+            </div>
+
+            <div className="bottom">
+              <div className="colorOptions"></div>
+              <div className="colorOptions"></div>
+              <div className="colorOptions"></div>
+            </div>
         </div>
 
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 import "./home.scss";
 
 
 const Home = () => {
   return (<div className="home">
     <Sidebar />
-    <div className="homeContainer">COntainer</div>
+    <div className="homeContainer">
+      <Navbar />
+      Home Container
+    </div>
   </div>);
 }
 
