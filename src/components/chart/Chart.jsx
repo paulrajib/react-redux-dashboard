e66ddeec70d05618c9a100d8976ffa94c0afd193
Rajib_p <silicon.slice@gmail.com>
2022-10-05 18:49:52 +0600

    column sortable data table

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -51,7 +51,8 @@ const Chart = () => {
   return (
       <div className="chart">
         <div className="title">Last 6 Months (Revenue)</div>
-            <ResponsiveContainer width="100%" height="90%">
+            {/* <ResponsiveContainer width="100%" height="80%"> */}
+            <ResponsiveContainer width="95%" aspect={5/3}>
                 <AreaChart width={730} height={250} data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                     <defs>
                         <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
@@ -63,9 +64,9 @@ const Chart = () => {
                             <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
                         </linearGradient>
                     </defs>
-                    <XAxis dataKey="name" />
+                    <XAxis dataKey="name" stroke='gray'/>
                     <YAxis />
-                    <CartesianGrid strokeDasharray="3 3" />
+                    <CartesianGrid strokeDasharray="3 3" className='chartGrid'/>
                     <Tooltip />
                     <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
                     <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
