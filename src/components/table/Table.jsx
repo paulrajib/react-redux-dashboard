be81ffa7fa4b274dd64b1ac78bb9d92d9dcab6d6
Rajib_p <silicon.slice@gmail.com>
2022-10-08 19:13:10 +0600

    admn home tbl data formt changed TblCOntainer changed from sortable to non-

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -13,50 +13,95 @@ import { DataGrid } from '@mui/x-data-grid';
 const List = () => {
 
 
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 70 },
-        { field: 'firstName', headerName: 'First name', width: 130 },
-        { field: 'lastName', headerName: 'Last name', width: 130 },
+    const rows = [
         {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 90,
+          id: 1143155,
+          product: "Acer Nitro 5",
+          img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+          customer: "John Smith",
+          date: "1 March",
+          amount: 785,
+          method: "Cash on Delivery",
+          status: "Approved",
         },
         {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+          id: 2235235,
+          product: "Playstation 5",
+          img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
+          customer: "Michael Doe",
+          date: "1 March",
+          amount: 900,
+          method: "Online Payment",
+          status: "Pending",
         },
-      ];
-      
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+        {
+          id: 2342353,
+          product: "Redragon S101",
+          img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
+          customer: "John Smith",
+          date: "1 March",
+          amount: 35,
+          method: "Cash on Delivery",
+          status: "Pending",
+        },
+        {
+          id: 2357741,
+          product: "Razer Blade 15",
+          img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
+          customer: "Jane Smith",
+          date: "1 March",
+          amount: 920,
+          method: "Online",
+          status: "Approved",
+        },
+        {
+          id: 2342355,
+          product: "ASUS ROG Strix",
+          img: "https://m.media-amazon.com/images/I/81hH5vK-MCL._AC_UY327_FMwebp_QL65_.jpg",
+          customer: "Harold Carol",
+          date: "1 March",
+          amount: 2000,
+          method: "Online",
+          status: "Pending",
+        },
     ];
 
     return (
-        <div style={{ height: 400, width: '100%' }} className="table">
-            <DataGrid
-                className="cellWrapper"
-                rows={rows}
-                columns={columns}
-                pageSize={5}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-            />
-        </div>
+        <TableContainer component={Paper} className="table">
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell className="tableCell">Tracking ID</TableCell>
+                    <TableCell className="tableCell">Product</TableCell>
+                    <TableCell className="tableCell">Customer</TableCell>
+                    <TableCell className="tableCell">Date</TableCell>
+                    <TableCell className="tableCell">Amount</TableCell>
+                    <TableCell className="tableCell">Payment Method</TableCell>
+                    <TableCell className="tableCell">Status</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow key={row.id}>
+                        <TableCell className="tableCell">{row.id}</TableCell>
+                        <TableCell className="tableCell">
+                        <div className="cellWrapper">
+                        <img src={row.img} alt="" className="image" />
+                        {row.product}
+                        </div>
+                        </TableCell>
+                        <TableCell className="tableCell">{row.customer}</TableCell>
+                        <TableCell className="tableCell">{row.date}</TableCell>
+                        <TableCell className="tableCell">{row.amount}</TableCell>
+                        <TableCell className="tableCell">{row.method}</TableCell>
+                        <TableCell className="tableCell">
+                            <span className={`status ${row.status}`}>{row.status}</span>
+                        </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     )
 }
 
