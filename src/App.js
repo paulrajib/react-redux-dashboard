7d55f8bcb7f1a4c2da293cc8f48704f2b699d6c5
Rajib_p <silicon.slice@gmail.com>
2022-10-16 14:14:10 +0600

    git theming

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { productInputs, userInputs } from "./formSource.js";
+import { productInputs, userInputs } from "./formSource";
+import "./style/dark.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app dark">
       <BrowserRouter>
         <Routes>
           <Route path="/">
--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -24,11 +24,21 @@ const Datatable = () => {
   ];
   return (
     <div className="datatable">
+      <div className="datatableTitle">
+        Add New User
+        <Link
+          to="/users/new"
+          style={{ textDecoration: "none" }}
+          className="link"
+        >
+          Add New
+        </Link>
+      </div>
       <DataGrid
         rows={userRows}
         columns={userColumns.concat(actionColumn)}
-        pageSize={9}
-        rowsPerPageOptions={[9]}
+        pageSize={6}
+        rowsPerPageOptions={[6]}
         checkboxSelection
       />
     </div>
